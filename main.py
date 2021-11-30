3437b6a12e5babd4aa324aed24020f9fd55d7637
Emre Ercelebi <30380456+emreercelebi@users.noreply.github.com>
2021-11-30 16:09:42 -0500

    update values of last-indices.json after each run, remove commented code and debugging logs

--- main.py
+++ main.py
@@ -28,7 +28,6 @@ for config in configs:
   #get last timestamp to use as starting point in gemini api call
   last_index = last_indices[config["index_key"]]
   last_timestamp = sheets.get_last_timestamp(sheet, config["sheet_name"], last_index)
-  print(last_timestamp)
 
   my_trades = get_trades(config["symbol"], last_timestamp)
   if len(my_trades) <= 1:
@@ -38,17 +37,15 @@ for config in configs:
   my_trades.reverse()
   #since gemini api returns all transaction on or after provided timestamp, we remove the first so we only have transactions that occurred after the provided timestamp
   my_trades = my_trades[1:]
-  print(my_trades)
 
+  #write data to spreadsheet
   sheets.write_to_sheet(sheet, config["sheet_name"], last_index, my_trades)
 
-# f = open("last-indices.json")
-# last_indices = json.load(f)
-# f.close()
+  #update last-indices.json with new values for the last indices
+  last_indices[config["index_key"]] = last_index + len(my_trades)
+  f = open("last-indices.json", "w")
+  json.dump(last_indices, f)
 
-
-# f = open("last-indices.json", "w")
-# json.dump(last_indices, f)
 
 
 
