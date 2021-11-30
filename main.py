f6b4c3a67d56ec81eabbe1377431870199dcd971
Emre Ercelebi <30380456+emreercelebi@users.noreply.github.com>
2021-11-30 13:53:30 -0500

    extract gemini fetching into its own file, use last-indices json file for quick retrieval of last entry in spreadsheets, write get_last_timestamp

--- main.py
+++ main.py
@@ -1,45 +1,38 @@
-import secrets #separate file to store api key and secret
-import requests
+from trades import get_trades
+from secrets import BTC_SHEET_NAME, ETH_SHEET_NAME
+import sheets
+
 import json
-import base64
-import hmac
-import hashlib
-import datetime, time
 
-'''
-Make request to gemini to get trades. Ref: https://docs.gemini.com/rest-api/#private-api-invocation 
-'''
-def get_trades(symbol):
-  #import and assign api key and secret here
-  api_key = secrets.API_KEY
-  api_secret = secrets.API_SECRET
-
-  url = "https://api.gemini.com/v1/mytrades"
-  gemini_api_key = api_key
-  gemini_api_secret = api_secret.encode()
-
-  t = datetime.datetime.now()
-  payload_nonce =  str(int(time.mktime(t.timetuple())*1000))
-  payload =  {
-    "request": "/v1/mytrades",
-    "nonce": payload_nonce,
-    "symbol": symbol
+configs = [
+  {
+    "sheet_name": BTC_SHEET_NAME,
+    "symbol": "btcusd",
+    "index_key": "btc"
+  },
+  {
+    "sheet_name": ETH_SHEET_NAME,
+    "symbol": "ethusd",
+    "index_key": "eth"
   }
-  encoded_payload = json.dumps(payload).encode()
-  b64 = base64.b64encode(encoded_payload)
-  signature = hmac.new(gemini_api_secret, b64, hashlib.sha384).hexdigest()
+]
 
-  request_headers = {
-    'Content-Type': "text/plain",
-    'Content-Length': "0",
-    'X-GEMINI-APIKEY': gemini_api_key,
-    'X-GEMINI-PAYLOAD': b64,
-    'X-GEMINI-SIGNATURE': signature,
-    'Cache-Control': "no-cache"
-  }
+sheet = sheets.get_sheet()
 
-  response = requests.post(url, headers=request_headers)
-  return response.json()
+for config in configs:
+  f = open("last-indices.json")
+  last_indices = json.load(f)
+  last_index = last_indices[config["index_key"]]
+  last_timestamp = sheets.get_last_timestamp(sheet, config["sheet_name"], last_index)
+  print(last_timestamp)
+
+# f = open("last-indices.json")
+# last_indices = json.load(f)
+# f.close()
+
+
+# f = open("last-indices.json", "w")
+# json.dump(last_indices, f)
 
 
 my_trades = get_trades("btcusd")
